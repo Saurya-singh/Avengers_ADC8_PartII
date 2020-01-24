fa1410c0e17b58406b7a42a3db814f459f4244fa
Roshan Shrestha <roshanshretha768@gmail.com>
2020-01-24 14:15:27 +0545

    create url path and view function for signup

--- FutsalManagementSystem/login_signup/urls.py
+++ FutsalManagementSystem/login_signup/urls.py
@@ -2,3 +2,7 @@ from django.contrib import admin
 from .views import *
 from django.urls import path
 
+urlpatterns = [
+    path('signup/',view_signup),
+    
+]
--- FutsalManagementSystem/login_signup/views.py
+++ FutsalManagementSystem/login_signup/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template import Template,Context
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.models import User
 # Create your views here.
+
+def view_signup(request):
+    if request.method =="GET":
+        return render(request,'signup.html')
+    else:
+        user = User.objects.create_user(username=request.POST['username'],password=request.POST['pwd'],email=request.POST['email'])
+        user.save()
+        return HttpResponse("Signup Successful")
