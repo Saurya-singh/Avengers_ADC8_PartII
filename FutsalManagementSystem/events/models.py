2862e1d1e2a599790cb3a9ddf15a900185868434
sudiplamatamang <sudiptamang123>
2020-01-24 17:09:36 +0545

    model relationship with screenshot

--- FutsalManagementSystem/events/models.py
+++ FutsalManagementSystem/events/models.py
@@ -2,23 +2,15 @@ from django.db import models
 
 # Create your models here.
 # creating objects for table in database 
-class Event(models.Model):
-    event_name = models.CharField('Event Name',max_length=100)
-    venue = models.CharField('Venue',max_length=100)
-    event_date = models.DateField('Event Date')
-    manager = models.CharField('Manager Name', max_length=100)
+class Payment(models.Model):
+    team_name = models.CharField('Event Name',max_length=100)
+    duration = models.IntegerField('Duration')
+    charge = models.IntegerField('charge')
+    bill = models.CharField('Bill', max_length=100)
 
     def __str__(self):
-        return str(self.id) + " " + self.event_name + " " + self.venue+ " " + str(self.event_date) + " " + self.manager
+        return str(self.id) + " " + self.team_name + " " + str(self.duration)+ " " + str(self.charge) + " " + self.bill
 
-
-class Field(models.Model):
-    field_name = models.CharField('Field Name',max_length=100)
-    field_address = models.CharField('Field Address',max_length=100)
-    field_contact = models.IntegerField('Field Contact')
-    customer_field=models.ManyToManyField(Customer)
-    def __str__(self):
-        return str(self.id) + " " + self.field_name + " " + self.field_address+ " " + str(self.field_contact) 
 
 
 class Customer(models.Model):
@@ -30,8 +22,32 @@ class Customer(models.Model):
         return str(self.id) + " " + self.team_name + " " + self.address+ " " + str(self.contact) 
 
 
+
+
+class Event(models.Model):
+    event_name = models.CharField('Event Name',max_length=100)
+    venue = models.CharField('Venue',max_length=100)
+    event_date = models.DateField('Event Date')
+    manager = models.CharField('Manager Name', max_length=100)
+
+    def __str__(self):
+        return str(self.id) + " " + self.event_name + " " + self.venue+ " " + str(self.event_date) + " " + self.manager
+
+
+
+
+class Field(models.Model):
+    field_name = models.CharField('Field Name',max_length=100)
+    field_address = models.CharField('Field Address',max_length=100)
+    field_contact = models.IntegerField('Field Contact')
+    customer_field=models.ManyToManyField(Customer)
+    def __str__(self):
+        return str(self.id) + " " + self.field_name + " " + self.field_address+ " " + str(self.field_contact) 
+
+
+
 class Booking(models.Model):
-    duration = models.IntegerField('Duration',max_length=100)
+    duration = models.IntegerField('Duration')
     charge = models.IntegerField('charge')
     payment = models.IntegerField('Payment')
     team_name = models.CharField('Event Name',max_length=100)
@@ -39,17 +55,9 @@ class Booking(models.Model):
     def __str__(self):
         return str(self.id) + " " + str(self.duration) + " " + str(self.charge)+ " " + str(self.payment) + " " + self.team_name
 
-class Payment(models.Model):
-    team_name = models.CharField('Event Name',max_length=100)
-    duration = models.IntegerField('Duration',max_length=100)
-    charge = models.IntegerField('charge')
-    bill = models.CharField('Bill', max_length=100)
-
-    def __str__(self):
-        return str(self.id) + " " + self.team_name + " " + str(self.duration)+ " " + str(self.charge) + " " + self.bill
 
 class Staff(models.Model):
-    contact = models.IntegerField('Contact',max_length=100)
+    contact = models.IntegerField('Contact')
     name = models.CharField('Name',max_length=100)
     address = models.CharField('Address', max_length=100)
     staff_field=models.ForeignKey(Field, on_delete=models.CASCADE)
