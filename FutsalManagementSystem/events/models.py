1a8ae555cdd74817a7e6c76d2c88ed0cb7cf5f64
sudiplamatamang <sudiptamang123>
2020-01-24 14:16:14 +0545

    added relation in models

--- FutsalManagementSystem/events/models.py
+++ FutsalManagementSystem/events/models.py
@@ -12,11 +12,11 @@ class Event(models.Model):
         return str(self.id) + " " + self.event_name + " " + self.venue+ " " + str(self.event_date) + " " + self.manager
 
 
-class Admin(models.Model):
+class Field(models.Model):
     field_name = models.CharField('Field Name',max_length=100)
     field_address = models.CharField('Field Address',max_length=100)
     field_contact = models.IntegerField('Field Contact')
-    
+    customer_field=models.ManyToManyField(Customer)
     def __str__(self):
         return str(self.id) + " " + self.field_name + " " + self.field_address+ " " + str(self.field_contact) 
 
@@ -25,7 +25,7 @@ class Customer(models.Model):
     team_name = models.CharField('Team Name',max_length=100)
     address = models.CharField('Address',max_length=100)
     contact = models.IntegerField('Contact ')
-    
+    customer_payment=models.ManyToManyField(Payment)
     def __str__(self):
         return str(self.id) + " " + self.team_name + " " + self.address+ " " + str(self.contact) 
 
@@ -35,7 +35,7 @@ class Booking(models.Model):
     charge = models.IntegerField('charge')
     payment = models.IntegerField('Payment')
     team_name = models.CharField('Event Name',max_length=100)
-
+    customer_booking=models.ForeignKey(Customer, on_delete=models.CASCADE)
     def __str__(self):
         return str(self.id) + " " + str(self.duration) + " " + str(self.charge)+ " " + str(self.payment) + " " + self.team_name
 
@@ -52,6 +52,7 @@ class Staff(models.Model):
     contact = models.IntegerField('Contact',max_length=100)
     name = models.CharField('Name',max_length=100)
     address = models.CharField('Address', max_length=100)
+    staff_field=models.ForeignKey(Field, on_delete=models.CASCADE)
 
     def __str__(self):
         return str(self.id) + " " + str(self.contact)+  " " + self.team_name + " " + self.address
